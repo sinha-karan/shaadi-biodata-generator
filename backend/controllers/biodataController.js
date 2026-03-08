2aa962605b67ea7fc662f8ab924c6ee03ab456e5
Karan Sinha <0308karan.sinha@gmail.com>
2026-03-09 03:07:54 +0530

    Implement biodata update and delete functionality with validation checks

--- backend/controllers/biodataController.js
+++ backend/controllers/biodataController.js
@@ -80,6 +80,42 @@ export const updateBiodata = async (req, res) => {
   try {
     const biodataId = req.params.id
     const userId = req.userId;
+    const {templateId, personalInfo, contactInfo, professionalInfo, familyInfo, hobbies, expectations, profilePhoto} = req.body;
+
+    if (Object.keys(req.body || {}).length === 0) {
+      return res.status(403).json({
+        message: "No info is provided"
+      })
+    }
+
+    if (!biodataId) {
+      return res.status(400).send("Required fields are missing")
+    }
+
+    const biodata = await Biodata.findById(biodataId)
+
+    if (biodata === null) {
+      return res.status(404).json({
+        message: "Biodata doesn't exist"
+      })
+    }
+
+    if (biodata.userId.toString() !== userId) {
+      return res.status(403).json({
+        message: "unauthorized access"
+      })
+    }
+
+    const updatedBiodata = await Biodata.findByIdAndUpdate(
+      biodataId
+    ,{
+      templateId, personalInfo, contactInfo, professionalInfo, familyInfo, hobbies, expectations, profilePhoto
+    })
+
+    res.status(200).json({
+      message: "Biodata updated successfully",
+      biodata: updatedBiodata
+    })
 
   } catch (error) {
     console.error("An error occurred", error);
@@ -91,6 +127,30 @@ export const deleteBiodata = async (req, res) => {
   try {
     const biodataId = req.params.id
     const userId = req.userId;
+
+    if (!biodataId) {
+      return res.status(400).send("Required fields are missing")
+    }
+
+    const biodata = await Biodata.findById(biodataId)
+
+    if (biodata === null) {
+      return res.status(404).json({
+        message: "Biodata doesn't exist"
+      })
+    }
+
+    if (biodata.userId.toString() !== userId) {
+      return res.status(403).json({
+        message: "unauthorized access"
+      })
+    }
+
+    await Biodata.findByIdAndDelete(biodataId);
+
+    res.status(200).json({
+      message: "Biodata deleted successfully"
+    })
 
   } catch (error) {
     console.error("An error occurred", error);
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import router from './routes/authRoutes.js';
+import authRouter from './routes/authRoutes.js';
+import biodataRouter from './routes/biodataRoutes.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", router);
+app.use("/api/auth", authRouter);
+app.use("/api/biodata", biodataRouter)
 
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
